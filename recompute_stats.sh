ae13c8af36dc1ceb4f9592989569127757dde5e4
Satrio Adi Rukmono <satrio@rukmono.id>
2026-06-08 10:43:01 +0700

    Revise stats

--- recompute_stats.sh
+++ recompute_stats.sh
@@ -1,5 +1,5 @@
 rm -r /mnt/data/repos/stats/
-python compute_graph_stats.py --graphs-dir /mnt/data/repos/graphs/java_v3.1/ --lang java --out-dir /mnt/data/repos/stats/java/
-python compute_graph_stats.py --graphs-dir /mnt/data/repos/graphs/csharp/ --lang csharp --out-dir /mnt/data/repos/stats/csharp/
-python compute_graph_stats.py --graphs-dir /mnt/data/repos/graphs/cpp/ --lang cpp --out-dir /mnt/data/repos/stats/cpp/
+python compute_graph_stats.py --graphs-dir /mnt/data/repos/a-graphs/java/dp/ --lang java --out-dir /mnt/data/repos/stats/java/
+python compute_graph_stats.py --graphs-dir /mnt/data/repos/a-graphs/csharp/dp/ --lang csharp --out-dir /mnt/data/repos/stats/csharp/
+python compute_graph_stats.py --graphs-dir /mnt/data/repos/a-graphs/cpp/dp/ --lang cpp --out-dir /mnt/data/repos/stats/cpp/
 cp  -r /mnt/data/repos/stats ~/Code/arcana-studies
